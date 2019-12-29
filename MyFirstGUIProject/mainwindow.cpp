1beaf3ec819a7c77614135a9a44c7cfaa7c60ae7
Alex-tech-it <58144587+Alex-tech-it@users.noreply.github.com>
2019-12-29 14:50:17 +0300

    f

--- MyFirstGUIProject/mainwindow.cpp
+++ MyFirstGUIProject/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QDebug>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +16,15 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString Login = ui->login->text() ;
+    QString Password = ui->password->text();
+    if( Login == "Гольчевский" && Password == "Сдал все программы"){
+        QMessageBox::information(this,"Вход","Вы успешно вошли в Гольчевского");
+    }else{
+        QMessageBox::warning(this,"Вход","Вас спалил Гольчевский, вы отчислены! Собирайте свои манатки и "
+                                         "проваливаете");
+    }
+}
